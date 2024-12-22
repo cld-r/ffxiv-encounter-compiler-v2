f26156fffff547d64a1b23fd38ebaaafbb7bdeb7
David Tham <david.tham@uzh.ch>
2024-12-22 21:42:37 +0100

    Remove config.conf

--- fflogs_utils.py
+++ fflogs_utils.py
@@ -1,5 +1,8 @@
 import requests
 
+FRU_FILTER_EXPRESSION = "ability.id IN (40140, 40197, 40179, 40212, 40259, 40266, 40269, 40301, 40298, 40306, 40327)"
+FRU_FILTER_EXPRESSION_2 = "ability.name IN ('Fall of Faith', 'Diamond Dust', 'Mirror, Mirror', 'Light Rampant', 'Endless Ice Age', 'Ultimate Relativity', 'Spell-In-Waiting', 'Darklit Dragonsong', 'Crystallize Time', 'Fulgent Blade', 'Paradise Lost')"
+
 def get_fflogs_access_token(client_id, client_secret):
     url = 'https://www.fflogs.com/oauth/token'
     data = {
@@ -14,6 +17,36 @@ def get_fflogs_access_token(client_id, client_secret):
     else:
         print(f"Failed to get access token: {response.status_code} - {response.text}")
         return None
+
+
+def get_fflogs_events(report_id, fight_id, access_token):
+    url = 'https://www.fflogs.com/api/v2/client'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    query = """
+    query($reportID: String!, $fightID: Int!, $filterExpression: String!) {
+        reportData {
+            report(code: $reportID) {
+                events(fightIDs: [$fightID], hostilityType: Enemies, useAbilityIDs:false, filterExpression: $filterExpression) {
+									data
+						}
+					}
+        }
+    }
+    """
+    variables = {
+        'reportID': report_id,
+        'fightID': fight_id,
+        'filterExpression': FRU_FILTER_EXPRESSION
+    }
+
+    response = requests.post(url, headers=headers, json={'query': query, 'variables': variables})
+
+    if response.status_code == 200:
+        response_data = response.json()
+        ability_names = list({event['ability']['name'] for event in response_data['data']['reportData']['report']['events']['data']})
+        return ability_names
 
 def get_fflogs_report(report_id, access_token):
     url = 'https://www.fflogs.com/api/v2/client'
@@ -36,12 +69,14 @@ def get_fflogs_report(report_id, access_token):
     """
     variables = {
         'reportID': report_id
-    }
+    }        
 
     response = requests.get(url, headers=headers, json={'query': query, 'variables': variables})
     if response.status_code == 200:
-        print(f"Successfully got report data: {response.status_code}")
-        print(f"Response Text: {response.text}")
+        print(f"Successfully fetched report data: {response.status_code}")
+        response_data = response.json()
+        fight_ids = [fight['id'] for fight in response_data['data']['reportData']['report']['fights'] if fight['name'] == 'Futures Rewritten']
+        return fight_ids
     else:
         print(f"Failed to get report data: {response.status_code} - {response.text}")
         return None
@@ -51,6 +86,9 @@ def handle_report(url, client_id, client_secret):
     access_token = get_fflogs_access_token(client_id, client_secret)
 
     if access_token:
-        report_data = get_fflogs_report(report_id, access_token)
-        if report_data:
-            print(report_data)
+        fight_ids = get_fflogs_report(report_id, access_token)
+        print(f"Fetched fight ids: {fight_ids}")
+        if fight_ids:
+            for fight_id in fight_ids:
+                ability_game_ids = get_fflogs_events(report_id, fight_id, access_token)
+                print(f"Events for fight: {fight_id}, {ability_game_ids}")
