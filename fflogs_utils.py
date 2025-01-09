7d35dd201e198daa58669247e8b9059fbdfa9c33
David Tham <tham.david@gmx.ch>
2025-01-09 15:31:16 +0100

    Refactor fflogs report functions to track wipe IDs and total kills, and update query parameters accordingly

--- fflogs_utils.py
+++ fflogs_utils.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from time_utils import ms_to_datetime, ms_to_hhmmss
 from ultimate import FRU
 
@@ -16,24 +17,23 @@ def get_fflogs_access_token(client_id, client_secret):
     else:
         print(f"Failed to get access token: {response.status_code} - {response.text}")
         return None
-    
 
 def get_latest_event(priority_list, events):
     for ability in priority_list:
         if ability in events:
             return ability
-    return priority_list[-1]
+    return priority_list[-1] # Return the last ability in the list if none of the priority abilities are found
 
-def get_fflogs_events(report_id, fight_id, access_token, priority_list, filter_expression):
+def get_fflogs_events(report_id, wipe_id, access_token, priority_list, filter_expression):
     url = 'https://www.fflogs.com/api/v2/client'
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
     query = """
-    query($reportID: String!, $fightID: Int!, $filterExpression: String!) {
+    query($reportID: String!, $wipeID: Int!, $filterExpression: String!) {
         reportData {
             report(code: $reportID) {
-                events(fightIDs: [$fightID], hostilityType: Enemies, useAbilityIDs:false, filterExpression: $filterExpression) {
+                events(fightIDs: [$wipeID], hostilityType: Enemies, useAbilityIDs:false, filterExpression: $filterExpression) {
 									data
 						}
 					}
@@ -42,7 +42,7 @@ def get_fflogs_events(report_id, fight_id, access_token, priority_list, filter_e
     """
     variables = {
         'reportID': report_id,
-        'fightID': fight_id,
+        'wipeID': wipe_id,
         'filterExpression': filter_expression
     }
 
@@ -61,31 +61,35 @@ def get_fflogs_report(report_id, access_token):
         'Authorization': f'Bearer {access_token}'
     }
     query = """
-    query($reportID: String!) {
+    query($reportID: String!, $killType: KillType) {
         reportData {
             report(code: $reportID) {
                 startTime
                 endTime
-                fights {
+                fights(killType: $killType) {
                     id
                     startTime
                     endTime
                     name
+                    kill
                 }
             }
         }
     }
     """
     variables = {
-        'reportID': report_id
+        'reportID': report_id,
+        'killType': 'Encounters'
     }        
 
     response = requests.get(url, headers=headers, json={'query': query, 'variables': variables})
     if response.status_code == 200:
         print(f"Successfully fetched report data: {response.status_code}")
         response_data = response.json()
+        print(json.dumps(response_data, indent=2))
 
-        fight_ids = []
+        wipe_ids = []
+        kill_count = 0
         longest_pull_duration = 0
         pull_duration_sum = 0
         raid_start_time = None
@@ -96,9 +100,13 @@ def get_fflogs_report(report_id, access_token):
         for fight in response_data['data']['reportData']['report']['fights']:
             pull_duration = fight['endTime'] - fight['startTime']
 
-            # Only include fights from the Ultimate and that are longer than 20 seconds
-            if fight['name'] == 'Futures Rewritten' and pull_duration >= 20000:
-                fight_ids.append(fight['id'])
+            # Only include fights from the Ultimate
+            if fight['name'] == 'Futures Rewritten':
+                if fight['kill']:
+                    kill_count +=1
+                else:
+                    wipe_ids.append(fight['id'])
+
                 pull_duration_sum += pull_duration
 
                 if raid_start_time is None:
@@ -107,13 +115,13 @@ def get_fflogs_report(report_id, access_token):
                     raid_end_time = fight['endTime']
                 if pull_duration > longest_pull_duration:
                     longest_pull_duration = pull_duration
-        
-        average_pull_duration = round(pull_duration_sum / len(fight_ids))
-        pull_count = len(fight_ids)
+
+        pull_count = len(wipe_ids) + kill_count
+        average_pull_duration = round(pull_duration_sum / pull_count)
         adjusted_raid_start_time = report_start_time + raid_start_time
         adjusted_raid_end_time = report_start_time + raid_end_time
 
-        return fight_ids, pull_count, adjusted_raid_start_time, adjusted_raid_end_time, longest_pull_duration, average_pull_duration, pull_duration_sum
+        return wipe_ids, pull_count, kill_count, adjusted_raid_start_time, adjusted_raid_end_time, longest_pull_duration, average_pull_duration, pull_duration_sum
     else:
         print(f"Failed to get report data: {response.status_code} - {response.text}")
         return None
@@ -125,15 +133,15 @@ def generate_report_summary(url, client_id, client_secret):
     ult = FRU()
 
     if access_token:
-        fight_ids, pull_count, raid_start_time, raid_end_time, longest_pull_duration, average_pull_duration, pull_duration_sum = get_fflogs_report(report_id, access_token)
+        wipe_ids, pull_count, kill_count, raid_start_time, raid_end_time, longest_pull_duration, average_pull_duration, pull_duration_sum = get_fflogs_report(report_id, access_token)
         
         raid_duration = raid_end_time - raid_start_time
 
         latest_event_counts = {event: 0 for event in ult.get_priority_list()}
 
-        if fight_ids:
-            for fight_id in fight_ids:
-                latest_event = get_fflogs_events(report_id, fight_id, access_token, ult.get_priority_list(), ult.get_filter_expression())
+        if wipe_ids:
+            for wipe_id in wipe_ids:
+                latest_event = get_fflogs_events(report_id, wipe_id, access_token, ult.get_priority_list(), ult.get_filter_expression())
                 if latest_event in latest_event_counts:
                     latest_event_counts[latest_event] += 1
 
@@ -143,7 +151,7 @@ def generate_report_summary(url, client_id, client_secret):
             f"{ult.get_long_name()}\n\n"
             f"Report ID:  \t\t{report_id}\n"
             f"Total pulls:\t\t{pull_count}\n"
-            f"Total kills:\t\t0\n"
+            f"Total kills:\t\t{kill_count}\n"
             f"Raid start: \t\t{ms_to_datetime(raid_start_time)}\n"
             f"Raid end:   \t\t{ms_to_datetime(raid_end_time)}\n"
             f"Raid duration:  \t{ms_to_hhmmss(raid_duration)}\n"
